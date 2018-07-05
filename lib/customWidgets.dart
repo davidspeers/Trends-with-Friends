a3c4eef4e4fab2179f5c3d6abeeb17ecc7f03d56
davidspeers <davidspeers25@outlook.com>
2018-07-05 22:13:44 +0100

    Made Back Button Design iOS Compatible

--- lib/customWidgets.dart
+++ lib/customWidgets.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 Widget homeIcon(BuildContext context) {
-  return new IconButton(
-    icon: new Icon(
-      Icons.arrow_back,
-      color: Colors.white
-    ),
-    onPressed: () {
-      Navigator.popUntil(context, ModalRoute.withName('/'));
+  switch(Theme.of(context).platform) {
+    case TargetPlatform.iOS: {
+      return new IconButton(
+          icon: new Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white
+          ),
+          onPressed: () {
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+          }
+      );
     }
-  );
+
+    default: {
+      return new IconButton(
+          icon: new Icon(
+              Icons.arrow_back,
+              color: Colors.white
+          ),
+          onPressed: () {
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+          }
+      );
+    }
+  }
+
 }
